a44fd084417d850987756cae28c00812c23f21be
SaiChilkoori09 <22417@students.mrgs.school.nz>
2026-04-22 10:42:33 +1200

    added title, screensize and background

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #Import GUI models
 import tkinter as tk
 
+class PlannerApp:
+    def __init__(self, root):
+        self.root = root
+
+#For the  title, screen size and background
+        self.root.title("FitPlan")
+        self.root.geometry("1024x600")
+        self.root.configure(bg="#66738b")
 
