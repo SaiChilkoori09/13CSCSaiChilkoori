3b6ed18d8d4c3584b99324c132c0cfb0c20533bc
SaiChilkoori09 <22417@students.mrgs.school.nz>
2026-05-04 13:18:32 +1200

    ading title and its position for home page and font type and bold

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ window.title("FitPlan")
 
 window.config(background="#66738b")
 
-label = Label(window,text="FitPlan",font=('Canva Sans',92,'bold'))
+label = Label(window,
+              text="FitPlan",
+              font=('Canva Sans',92,'bold'),
+              fg='white',
+              bg='#66738b'
+              )
+
 label.place(x=0, y=0)
 
 window.mainloop()
