c10fa7808081e90810787ad4d4bef3778cb6afb1
SaiChilkoori09 <22417@students.mrgs.school.nz>
2026-05-04 13:02:52 +1200

    basic GUI

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-#Import GUI models
-import tkinter as tk
+from tkinter import *
 
-class PlannerApp:
-    def __init__(self, root):
-        self.root = root
+window = Tk()
+window.geometry("420x420")
+window.title("FitPlan")
 
-#For the  title, screen size and background
-        self.root.title("FitPlan")
-        self.root.geometry("1024x600")
-        self.root.configure(bg="#66738b")
-
+window.mainloop()
